0a6e288e4b83379fdb9645ac994191dd5923cf43
Hoyoul Youn <yhy0215@gmail.com>
2016-06-30 21:35:37 +1000

    change cipher key again becaue of the error on heroku

--- config/app.php
+++ config/app.php
@@ -94,7 +94,8 @@ return [
     'key' => env('APP_KEY'),
 
     //'cipher' => 'AES-256-CBC',
-    'cipher' => 'AES-128-CBC',
+    //'cipher' => 'AES-128-CBC',
+    'cipher' => MCRYPT_RIJNDAEL_128,
 
     /*
     |--------------------------------------------------------------------------
